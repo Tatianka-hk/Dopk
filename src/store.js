e928de388443a186cd98f4c6d8b1d1d1c84308c8
Tatianka-hk <t0681689150@gmail.com>
2022-01-22 12:16:46 +0200

    derived

--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
 import { writable } from "svelte/store";
+import { derived } from "svelte/store";
 
 export const isLoading = writable(false);
 export const errors = writable([]);
 export const url = writable("");
 export const recordType = writable("AAAA");
 export const result = writable({});
+export const urlString = derived(
+  [url, recordType],
+  ([$url, $recordType]) => `?url=${$url}&recordType=${$recordType}`
+);
